850f75bc1ec04cb217a1461a5ee9f659820f7c03
mathi-git03 <srimathiksl@gmail.com>
2024-03-01 19:35:34 +0530

    test update

--- src/app/home/home.jsx
+++ src/app/home/home.jsx
@@ -4,7 +4,7 @@ import HomeBannerAnimate from './homepage-animate'
 const HomePage = () => {
   return (
     <>
-    <div className='text-center text-blue-300 text-5xl pt-4'>3D object</div>
+    <div className='text-center text-blue-300 text-5xl pt-4'>3D object demo</div>
     <div className='flex h-[90vh] items-center'><HomeBannerAnimate /></div>
     </>
   )
